2d60fdd80d62cd301a8e16e8646e745fed34df6e
aderama2711 <ade.ramadha@gmail.com>
2023-02-15 17:33:38 +0700

    fix ilmi

--- topologi16nodes/makassar/run-makassar.sh
+++ topologi16nodes/makassar/run-makassar.sh
@@ -1,6 +1,6 @@
 sudo mkdir /var/lib/nlsr
 
-nfdc face create udp://192.169.1.73
+nfdc face create udp://192.168.1.73
 nfdc face create udp://192.168.1.77
 nfdc face create udp://192.168.1.86
 nfdc face create udp://192.168.1.90
