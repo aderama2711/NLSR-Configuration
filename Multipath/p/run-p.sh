39530e5dbcd644a65c7e51bfbb9f907be14e1af0
aderama2711 <ade.ramadha@gmail.com>
2023-05-01 18:33:11 +0700

    update multipath topology

--- Multipath/p/run-p.sh
+++ Multipath/p/run-p.sh
@@ -1,5 +1,7 @@
 sudo mkdir /var/lib/nlsr
 
-nfdc face create udp://192.168.1.32
-nfdc face create udp://192.168.1.36
+nfdc face create udp://192.168.1.16
+nfdc face create udp://192.168.1.20
+nfdc face create udp://192.168.1.24
+nfdc face create udp://192.168.1.28
 sudo NDN_LOG='nlsr.*=DEBUG' nlsr -f nlsr-p.conf
