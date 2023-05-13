ffdbc068e6b452553dc5670a5e93b76e9749b5dd
aderama2711 <ade.ramadha@gmail.com>
2023-05-13 12:15:26 +0700

    update

--- IP-NDN/bandung/run-bandung.sh
+++ IP-NDN/bandung/run-bandung.sh
@@ -1,4 +1,7 @@
 sudo mkdir /var/lib/nlsr
 
+nfdc face create udp://192.168.5.5
+nfdc face create udp://192.168.5.10
+nfdc face create udp://192.168.5.14
 nfdc face create udp://192.168.5.26
 sudo NDN_LOG='nlsr.*=DEBUG' nlsr -f nlsr-bandung.conf
