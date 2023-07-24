0b1121033474c4ac63d51352c0c21804aea6022a
Ade Aditya Ramadha <ade.ramadha@gmail.com>
2023-07-24 13:32:47 +0700

    Add files via upload

---
+++ IP-NDN Update/ambon/run-ambon.sh
@@ -0,0 +1,6 @@
+sudo mkdir /var/lib/nlsr
+
+nfdc face create udp://192.168.5.1
+nfdc face create udp://192.168.5.6
+nfdc face create udp://192.168.5.29
+sudo NDN_LOG='nlsr.*=DEBUG' nlsr -f nlsr-ambon.conf
